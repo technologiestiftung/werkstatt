9d74affec14c68cacf8f13d06a00a58a92c87956
julizet <juliazet@gmx.de>
2019-11-29 15:54:15 +0100

    added code

---
+++ codes_sketches/Gardening.ino
@@ -0,0 +1,39 @@
+int messwert=0; //Unter der Variablen "messwert" wird später der Messwert des Sensors gespeichert.
+int Pump=5;
+#define Sensor        A0 
+void setup() 
+
+{ ////Hier beginnt das Setup.
+
+  Serial.begin(9600); //Die Kommunikation mit dem seriellen Port wird gestartet. Das benötigt man, um sich den ausgelesenen Wert im serial monitor anzeigen zu lassen.
+  pinMode (5, OUTPUT);
+  pinMode(Sensor,INPUT_PULLUP); 
+   
+}
+
+void loop()
+
+{ //Hier beginnt der Hauptteil  
+
+ messwert=analogRead(Sensor); //Die Spannung an dem Sensor wird ausgelesen und unter der Variable „messwert“ gespeichert.
+
+  Serial.print("Feuchtigkeits-Messwert:"); //Ausgabe am Serial-Monitor: Das Wort „Feuchtigkeits-Messwert:"
+
+  Serial.println(messwert); //und im Anschluss der eigentliche Messwert.
+
+  delay(100); //Zum Schluss noch eine kleine Pause, damit nicht zu viele Zahlenwerte über den Serial-Monitor rauschen.
+if (messwert>960)
+{ 
+
+digitalWrite(Pump, HIGH); //...soll die Pumpe pumpen.
+}
+
+else //..ansonsten...
+
+{
+
+digitalWrite(Pump, LOW); //...soll die Pumpe aus sein.
+
+}
+
+}
